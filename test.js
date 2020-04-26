179527d461f760ae116ff6cf11fae0d76dad216c
zhangbi <zhangbi@huizhaofang.com>
2020-04-26 23:45:53 +0800

    算法测试

--- test.js
+++ test.js
@@ -74,3 +74,75 @@ Object.defineProperty(obj, 'a', {
 obj.a = 2
 console.log(obj);
 console.log(Object.keys(obj));
+
+var obj = {
+  left: null,
+  right: {
+    
+  }
+}
+var maxDepth = function (root) {
+  if (root === null) { //注意等号
+    return 0;
+  } else {
+    var leftDepth = 0;
+    var rightDepth = 0
+    if (root) {
+      console.log(root);
+      leftDepth = maxDepth(root.left)
+      console.log(leftDepth);
+      rightDepth = maxDepth(root.right)
+      console.log(rightDepth);
+    }
+    var childDepth = leftDepth > rightDepth ? leftDepth : rightDepth;
+
+    return childDepth + 1;//根节点不为空高度至少为1
+  }
+};
+let str = JSON.stringify(obj)
+console.log(str);
+let nodeNums = str.match(/\w+/g).length
+console.log(nodeNums);
+let deepNum = maxDepth(obj)
+console.log(deepNum);
+
+var myArr = [55, 12, 35, 5, 325, 18, 2, 9, 11, 5, 12, 35, 76, 89]
+var obj = {
+}
+myArr.forEach(item => {
+  let key = item + ''
+  obj[key] = {}
+  obj[key].value = item
+  obj[key].times = myArr.filter(i => i === item).length
+})
+
+console.log(obj);
+var newObj = {}
+for (const value of myArr) {
+  if (!newObj[value]) {
+    newObj[value] = {}
+    newObj[value]['value'] = value
+    newObj[value]['times'] = 1
+  } else {
+    newObj[value]['times'] += 1
+  }
+}
+console.log(newObj);
+
+var Obj2 = {}
+for (const key in myArr) {
+  let keys = myArr[key]
+  if (!Obj2[keys]) {
+    Obj2[keys] = {}
+    Obj2[keys]['value'] = keys
+    Obj2[keys]['times'] = 1
+  } else {
+    Obj2[keys]['times'] += 1
+  }
+}
+console.log(Obj2);
+
+
+
+
+

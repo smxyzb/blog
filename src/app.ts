31c682177d201dd9a18c6f7be69bea7bed3bb659
zb <zb@qq.com>
2022-06-09 00:13:10 +0800

    update

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,9 @@
-function add():void {
-  return undefined
+type Obj = {
+ [k:string]:unknown
 }
+
+type P = keyof Obj
+
+const a:P = 2
+const b:P = 'x'
+const c:P = true // 不能将类型“boolean”分配给类型“string | number”。

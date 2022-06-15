536526cc1549854948a4a686ae339871b47cc5b3
zhangbi <zhangbi@tal.com>
2022-06-15 18:49:44 +0800

    curry

--- src/index.js
+++ src/index.js
@@ -1,57 +1,26 @@
-function Iterator(params) {
-  let index = 0
-  return {
-    next(){
-      return index< params.length?{
-        value:params[index++],
-        done:false
-      }:{
-        value:undefined,
-        done:true
+function curryFn(fn) {
+  console.log('curryFn', fn.length);
+  function _curried(...args) {
+    return function () {
+      var allArgs = args.concat(Array.from(arguments));
+      console.log(allArgs);
+      if (allArgs.length >= fn.length) {
+        return fn.apply(this, allArgs);
+      } else {
+        return _curried.apply(fn, allArgs);
       }
     }
   }
+  return _curried
 }
 
-function generator(genF, resolve, reject) {
-  let result = null
-  try{
-    result = genF()
-  }
-  catch(error){}
-  console.log(result);
-
-  while (!result.done) {
-    result = generator(function myFn(params) {
-      return it.next(result.value)
-    })
-  }
-  return result
+var plus = function (a,b,c,d) {
+  // console.log(a,b,c,d);
+  return a+b+c+d;
 }
-
-function Await(syncFn) {
-  return generator(syncFn)
-}
-
-// ES5
-const it = Iterator([1, 2, 3])
-var g = function myFn() {
-  const next = it.next()
-  return next
-}
-// ES6 
-function* genF() {
-  yield 1
-  yield 2
-  yield 3
-  return 4
-}
-
-let res1 =  Await(g)
-console.log('res1');
-console.log(res1);
-let res2 = Await(genF)
-console.log('res2');
-console.log(res2);
+var add = curryFn(plus)
+console.log(add(1)(2)(3)(4));
+console.log(add(1,2,3)(4));
+console.log(add(1)(2)(4,5));
 
 
